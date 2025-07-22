68103e0fd246cfb095c54626b80a725ceea0ef59
bokchoy-99 <yuanzhilong99@163.com>
2025-07-22 11:07:36 +0000

    update

--- convert_onnx2om.sh
+++ convert_onnx2om.sh
@@ -5,10 +5,17 @@ op_name=$1
 onnx_name=$2
 
 # path variables
-
 TOOLS_OMG_PATH=/data/yzl/Workspace/DDK_tools/tools/tools_omg
 ONNX_PATH=/data/yzl/Workspace/PatchNet/output/onnx_output/test_npu/onnx/${op_name}
+if [ ! -d "$ONNX_PATH" ]; then
+    echo "Error: Directory $ONNX_PATH does not exist."
+    exit 1
+fi
+
 OM_PATH=/data/yzl/Workspace/PatchNet/output/onnx_output/test_npu/om/${op_name}
+if [ ! -d "$OM_PATH" ]; then
+    mkdir -p "$OM_PATH"
+fi
 
 # convert onnx to om
 # ./omg --model PatchNet_color.onnx --framework 5 --output ./PatchNet_color
